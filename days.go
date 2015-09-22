1ea26c2a8b0f3e48391388cc8161620d8cbe8809
guotie.9 <guotie.9@gmail.com>
2015-09-22 14:12:29 +0800

    add files

---
+++ days.go
@@ -0,0 +1,51 @@
+package days
+
+import (
+	"time"
+)
+
+// 比当前时间晚1个自然天的自然天的开始时间
+func Tomorrow(tm time.Time) (ret time.Time) {
+	return DaysAfter(tm, 1)
+}
+
+// 比当前时间早1个自然天的自然天的开始时间
+func Yesterday(tm time.Time) (ret time.Time) {
+	return DaysBefore(tm, 1)
+}
+
+func today(tm time.Time) (ret time.Time) {
+	_, offset := tm.Zone()
+	seconds := tm.Unix()
+	return time.Unix(seconds-(seconds+int64(offset))%86400, 0)
+}
+
+// 返回本自然天的开始时间
+func Today() (ret time.Time) {
+	return today(time.Now())
+}
+
+func Days(tm time.Time) int {
+	_, offset := tm.Zone()
+	return int((tm.Unix() + int64(offset)) / int64(86400))
+}
+
+// 两个时间之间的自然天差值
+func DaysBetween(t1, t2 time.Time) (days int) {
+	d1 := Days(t1)
+	d2 := Days(t2)
+	if d1 > d2 {
+		return d1 - d2
+	}
+	return d2 - d1
+}
+
+// N天以后的自然天的起始时间
+func DaysAfter(tm time.Time, days int) (ret time.Time) {
+	return
+}
+
+// N天之前的自然天的起始时间
+func DaysBefore(tm time.Time, days int) (ret time.Time) {
+	return
+}
---
+++ days_test.go
@@ -0,0 +1,146 @@
+package days
+
+import (
+	"testing"
+	"time"
+)
+
+var tms = []time.Time{
+	time.Date(2015, time.Month(9), 22, 0, 0, 0, 0, time.Local),
+	time.Date(2015, time.Month(9), 22, 0, 0, 0, 1, time.Local),
+	time.Date(2015, time.Month(9), 22, 7, 0, 0, 0, time.Local),
+	time.Date(2015, time.Month(9), 22, 7, 0, 1, 0, time.Local),
+	time.Date(2015, time.Month(9), 22, 8, 0, 0, 0, time.Local),
+	time.Date(2015, time.Month(9), 22, 8, 0, 1, 0, time.Local),
+	time.Date(2015, time.Month(9), 22, 15, 0, 0, 0, time.Local),
+	time.Date(2015, time.Month(9), 22, 15, 59, 1, 0, time.Local),
+	time.Date(2015, time.Month(9), 22, 16, 0, 0, 0, time.Local),
+	time.Date(2015, time.Month(9), 22, 16, 59, 1, 0, time.Local),
+	time.Date(2015, time.Month(9), 22, 23, 0, 0, 0, time.Local),
+	time.Date(2015, time.Month(9), 22, 23, 59, 1, 0, time.Local),
+}
+
+var daysdelta = []struct {
+	t1    time.Time
+	t2    time.Time
+	delta int
+}{
+	{
+		time.Date(2015, time.Month(9), 22, 0, 0, 0, 0, time.Local),
+		time.Date(2015, time.Month(9), 22, 0, 0, 0, 1, time.Local),
+		0,
+	},
+	{
+		time.Date(2015, time.Month(9), 22, 0, 0, 0, 0, time.Local),
+		time.Date(2015, time.Month(9), 22, 7, 0, 0, 1, time.Local),
+		0,
+	},
+	{
+		time.Date(2015, time.Month(9), 22, 0, 0, 0, 0, time.Local),
+		time.Date(2015, time.Month(9), 22, 8, 0, 0, 1, time.Local),
+		0,
+	},
+	{
+		time.Date(2015, time.Month(9), 22, 0, 0, 0, 0, time.Local),
+		time.Date(2015, time.Month(9), 22, 15, 0, 0, 1, time.Local),
+		0,
+	},
+	{
+		time.Date(2015, time.Month(9), 22, 0, 0, 0, 0, time.Local),
+		time.Date(2015, time.Month(9), 22, 16, 0, 0, 1, time.Local),
+		0,
+	},
+	{
+		time.Date(2015, time.Month(9), 22, 0, 0, 0, 0, time.Local),
+		time.Date(2015, time.Month(9), 22, 23, 59, 0, 1, time.Local),
+		0,
+	},
+	{
+		time.Date(2015, time.Month(9), 22, 0, 0, 0, 0, time.Local),
+		time.Date(2015, time.Month(9), 23, 0, 0, 0, 1, time.Local),
+		1,
+	},
+	{
+		time.Date(2015, time.Month(9), 22, 0, 0, 0, 0, time.Local),
+		time.Date(2015, time.Month(9), 23, 7, 0, 0, 1, time.Local),
+		1,
+	},
+	{
+		time.Date(2015, time.Month(9), 22, 0, 0, 0, 0, time.Local),
+		time.Date(2015, time.Month(9), 23, 8, 0, 0, 1, time.Local),
+		1,
+	},
+	{
+		time.Date(2015, time.Month(9), 22, 0, 0, 0, 0, time.Local),
+		time.Date(2015, time.Month(9), 23, 15, 0, 0, 1, time.Local),
+		1,
+	},
+	{
+		time.Date(2015, time.Month(9), 22, 0, 0, 0, 0, time.Local),
+		time.Date(2015, time.Month(9), 23, 16, 0, 0, 1, time.Local),
+		1,
+	},
+	{
+		time.Date(2015, time.Month(9), 22, 0, 0, 0, 0, time.Local),
+		time.Date(2015, time.Month(9), 23, 23, 59, 0, 1, time.Local),
+		1,
+	},
+	{
+		time.Date(2015, time.Month(9), 22, 0, 0, 0, 0, time.Local),
+		time.Date(2015, time.Month(9), 24, 0, 0, 0, 1, time.Local),
+		2,
+	},
+	{
+		time.Date(2015, time.Month(9), 22, 0, 0, 0, 0, time.Local),
+		time.Date(2015, time.Month(9), 24, 7, 0, 0, 1, time.Local),
+		2,
+	},
+	{
+		time.Date(2015, time.Month(9), 22, 0, 0, 0, 0, time.Local),
+		time.Date(2015, time.Month(9), 24, 8, 0, 0, 1, time.Local),
+		2,
+	},
+	{
+		time.Date(2015, time.Month(9), 22, 0, 0, 0, 0, time.Local),
+		time.Date(2015, time.Month(9), 24, 15, 0, 0, 1, time.Local),
+		2,
+	},
+	{
+		time.Date(2015, time.Month(9), 22, 0, 0, 0, 0, time.Local),
+		time.Date(2015, time.Month(9), 24, 16, 0, 0, 1, time.Local),
+		2,
+	},
+	{
+		time.Date(2015, time.Month(9), 22, 0, 0, 0, 0, time.Local),
+		time.Date(2015, time.Month(9), 24, 23, 59, 0, 1, time.Local),
+		2,
+	},
+}
+
+func TestToday(t *testing.T) {
+	t0 := Today()
+	for _, tm := range tms {
+		if today(tm) != t0 {
+			t.Logf("today of %v is %v, but should be %v\n", tm, today(tm), t0)
+			t.Fail()
+		}
+	}
+
+}
+
+func TestDays(t *testing.T) {
+	d0 := Days(tms[0])
+	for _, tm := range tms {
+		if Days(tm) != d0 {
+			t.Fail()
+		}
+	}
+}
+
+func TestDaysBetween(t *testing.T) {
+	for _, dd := range daysdelta {
+		if DaysBetween(dd.t1, dd.t2) != dd.delta {
+			t.Fail()
+		}
+	}
+}
