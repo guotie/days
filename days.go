461a4ff41cf0fbc3255155f57b64f67a2780fdbb
guotie <guotie.9@gmail.com>
2016-10-11 16:12:21 +0800

    golint

--- days.go
+++ days.go
@@ -4,34 +4,33 @@ import (
 	"time"
 )
 
-// 比当前时间晚1个自然天的自然天的开始时间
+// Tomorrow 比当前时间晚1个自然天的自然天的开始时间
 func Tomorrow(tm time.Time) (ret time.Time) {
-	return DaysAfter(tm, 1)
+	return After(tm, 1)
 }
 
-// 比当前时间早1个自然天的自然天的开始时间
+// Yesterday 比当前时间早1个自然天的自然天的开始时间
 func Yesterday(tm time.Time) (ret time.Time) {
-	return DaysBefore(tm, 1)
+	return Before(tm, 1)
 }
 
-func today(tm time.Time) (ret time.Time) {
-	_, offset := tm.Zone()
-	seconds := tm.Unix()
-	return time.Unix(seconds-(seconds+int64(offset))%86400, 0)
+func today(tm time.Time) time.Time {
+	return time.Date(tm.Year(), tm.Month(), tm.Day(), tm.Hour(), tm.Minute(), tm.Second(), 0, tm.Location())
 }
 
-// 返回本自然天的开始时间
+// Today 返回本自然天的开始时间
 func Today() (ret time.Time) {
 	return today(time.Now())
 }
 
+// Days 两个时间之间的自然天差值
 func Days(tm time.Time) int {
 	_, offset := tm.Zone()
 	return int((tm.Unix() + int64(offset)) / int64(86400))
 }
 
-// 两个时间之间的自然天差值
-func DaysBetween(t1, t2 time.Time) (days int) {
+// Between 两个时间之间的自然天差值
+func Between(t1, t2 time.Time) (days int) {
 	d1 := Days(t1)
 	d2 := Days(t2)
 	if d1 > d2 {
@@ -40,16 +39,14 @@ func DaysBetween(t1, t2 time.Time) (days int) {
 	return d2 - d1
 }
 
-// N天以后的自然天的起始时间
-func DaysAfter(tm time.Time, n int) (ret time.Time) {
+// After N天以后的自然天的起始时间
+func After(tm time.Time, n int) (ret time.Time) {
 	t := today(tm)
 	return time.Unix(t.Unix()+int64(86400*n), 0)
-	//return t.Add(time.Duration(n) * time.Duration(86400) * time.Second)
 }
 
-// N天之前的自然天的起始时间
-func DaysBefore(tm time.Time, n int) (ret time.Time) {
+// Before N天之前的自然天的起始时间
+func Before(tm time.Time, n int) (ret time.Time) {
 	t := today(tm)
 	return time.Unix(t.Unix()-int64(86400*n), 0)
-	//return t.Add(time.Duration(-n) * time.Duration(86400) * time.Second)
 }
--- tmr.go
+++ tmr.go
@@ -4,8 +4,10 @@ import (
 	"time"
 )
 
-// 与uv_timer_start类似
+// StartTimer 与uv_timer_start类似
 // uv_timer_start(tmr, first run time, loop time, fn)
+// n: 毫秒
+// m: 毫秒
 func StartTimer(n, m int64, fn func(args ...interface{}) error, args ...interface{}) *time.Timer {
 	tmr := time.NewTimer(time.Duration(n) * time.Millisecond)
 
@@ -15,6 +17,18 @@ func StartTimer(n, m int64, fn func(args ...interface{}) error, args ...interfac
 
 		if m != 0 {
 			for {
+				// 修复时间偏差
+				// 仅限小时定时和天定时
+				seconds := m
+				now := time.Now()
+				today := Today()
+				switch m {
+				case 3600:
+					seconds -= now.Unix() % 3600
+				case 86400:
+					seconds -= int64(now.Sub(today).Seconds())
+				}
+
 				tmr.Reset(time.Duration(m) * time.Millisecond)
 				<-tmr.C
 				fn(args)
